98032f0d9877125906c262cdcd9b2c65b7a98cb6
Taylor Collins <taylorcc33@gmail.com>
2022-01-22 15:56:32 -0700

    add list item, map over data hash

--- lib/yaru/list.rb
+++ lib/yaru/list.rb
@@ -2,5 +2,7 @@ def list
   file = File.read('./db/db.json')
   data_hash = JSON.parse(file)
 
-  puts data_hash
+  data_hash.map do |item|
+    puts "#{item["title"]} - #{item["complete"]}"
+  end
 end
